7854652492dfa5e428ce5e9ebc8d309213066c60
tms0052 <tms0052@mix.wvu.edu>
2022-02-08 14:53:28 -0500

    added albums for row component

--- src/app/marketplace/mock_listing_list.ts
+++ src/app/marketplace/mock_listing_list.ts
@@ -13,5 +13,37 @@ export const mock_listing_list = [
         shippingP: 5,
         price: 50.00,
         total: 55
+    },
+
+    {
+        img: "assets/marketAlbums/ArcticMonkeysAMCover.jpg",
+        title: "Arctic Monkeys - AM (LP, Album, Gat)",
+        label: "Domino",
+        catN: " WIGLP317",
+        mediaC: "Mint (M)",
+        sleeveC: "Mint(M)",
+        comment: "Domino; WIGLP317,Europe 2013, Gatefold, Insert, 180 Gram & Download (Our Ref: 0452148S) - SHIPS FROM OUR LONDON RECORD SHOPS - COME AND VISIT US - WE ARE NOW OPEN AGAIN - ISLINGTON N1 8LR, SHOREDITCH, E2 7DG or CROUCH END, N8 9DX",
+        username: "FlashbackLondon",
+        country: "United Kingdom",
+        ratingsN: 42078,
+        shippingP: 16,
+        price: 27.00,
+        total: 58.11
+    },
+
+    {
+        img: "assets/marketAlbums/BeastieBoysLicensedToIllCover.jpg",
+        title: "Beastie Boys - Licensed To Ill (LP, Album, Gat)",
+        label: "Epic, Epic",
+        catN: " 144.930, 144930",
+        mediaC: "Mint (M)",
+        sleeveC: "Very Good Plus(VG+)",
+        comment: "",
+        username: "Momento_Cultural",
+        country: "Brazil",
+        ratingsN: 390,
+        shippingP: 29.60,
+        price: 129.00,
+        total: 158.60
     }
 ]
