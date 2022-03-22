f52f48c45455d2054f43a4363f3e2e72e12cb827
tms0052 <tms0052@mix.wvu.edu>
2022-03-22 14:43:57 -0400

    firebase webapp setup and init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,26 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgRJ6mWVcFBdMuiMkYuU7s0p0aDq4uPp8",
+  authDomain: "discogs-app-dffb9.firebaseapp.com",
+  databaseURL: "https://discogs-app-dffb9-default-rtdb.firebaseio.com",
+  projectId: "discogs-app-dffb9",
+  storageBucket: "discogs-app-dffb9.appspot.com",
+  messagingSenderId: "104439111251",
+  appId: "1:104439111251:web:c31232cf59e48dd48370a6",
+  measurementId: "G-J8RVMNP032"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
