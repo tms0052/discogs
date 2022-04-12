5221816f8402a9aeffc12b5db6a267106c07e7e8
tms0052 <tms0052@mix.wvu.edu>
2022-04-12 17:23:16 -0400

    auth service added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core"
 import { NgForm } from "@angular/forms"
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service"
+import { AuthResponse } from "./authResponse"
 
 
 @Component({
@@ -7,8 +10,32 @@ import { NgForm } from "@angular/forms"
         templateUrl:  "auth.component.html"
     })
 export class AuthComponent{
+    public buttonClicked !: string;
+    private authObservable!: Observable<AuthResponse>;
+    
+    constructor(private authService:AuthService){
+    }
+
     public onSubmit(data: NgForm){
-        console.log("ButtonClicked")
+        console.log(this.buttonClicked)
         console.log(data.value)
+
+        if(this.buttonClicked=='signup'){
+            this.authObservable = this.authService.signup(data.value.email, data.value.password);
+        }
+        if(this.buttonClicked=='login'){
+            this.authObservable = this.authService.login(data.value.email, data.value.password);
+        }
+
+        this.authObservable.subscribe(
+            (data:AuthResponse)=>{
+                console.log(data)
+            },
+            error =>{
+                console.log(error.error);
+            }
+        );
+
+        data.resetForm();
     }
 }
